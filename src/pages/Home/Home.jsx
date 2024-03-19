913f59c03d9fa424b1b7272cdf5497793c8bd492
isalim079 <salimreza1025@gmail.com>
2024-03-19 21:23:58 +0600

    product data added with card

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,62 @@
-
+import { useEffect, useState } from "react";
 
 const Home = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const productsData = async () => {
+            const res = await fetch("https://fakestoreapi.com/products");
+            const data = await res.json();
+            setProducts(data);
+        };
+        productsData();
+    }, []);
+
+    // console.log(products);
+
     return (
-        <div>
-            Home
+        <div className="max-w-screen-xl mx-auto mb-20 mt-10">
+            <h1 className="text-center text-3xl font-semibold">
+                Cart your products
+            </h1>
+            <div className="flex justify-center mt-2">
+                <div className="border-2  mb-10 border-blue-950 w-72 "></div>
+            </div>
+            <div className="grid grid-cols-4 gap-12">
+                {products.map((product) => (
+                    <div key={product.id}>
+                        <div className="card card-compact h-full max-h-[420px]  shadow-xl">
+                            <figure>
+                                <img
+                                    className="h-full"
+                                    src={product?.image}
+                                    alt={product?.title}
+                                />
+                            </figure>
+                            <div className="card-body">
+                                <h2 className="card-title">{product?.title}</h2>
+                                <p>
+                                    {product?.description.length > 100
+                                        ? product?.description.slice(0, 100)
+                                        : product?.description}{" "}
+                                    {product?.description.length > 100 ? (
+                                        <span className="ml-2 text-xs underline">
+                                            read more...
+                                        </span>
+                                    ) : (
+                                        ""
+                                    )}
+                                </p>
+                                <div className="card-actions justify-end mt-4">
+                                    <button className="px-3 py-2 bg-green-500 text-white rounded-md">
+                                        Buy Now
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -1,10 +1,28 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
-       <div>
-         <div className="navbar bg-base-100 shadow-lg">
-            <a className="btn btn-ghost text-xl">daisyUI</a>
+        <div className=" bg-slate-100 py-2 shadow-lg">
+            <div className=" max-w-screen-xl mx-auto">
+                <div className="flex justify-between items-center">
+                    <div>
+                        <a href="/" className="btn btn-ghost text-xl font-semibold">
+                            ReTool
+                        </a>
+                    </div>
+                    <div className=" space-x-5">
+                        <Link to="/">Home</Link>
+                        <div className="indicator">
+                        <span className="indicator-item px-[6px] py-[1px] text-white bg-green-500 rounded-full text-xs">0</span>
+                        <Link to="/carts">Carts</Link>
+                        </div>
+                    </div>
+                    <div>
+                        <button className="px-3 py-2 bg-green-500 text-white rounded-md text-sm">Sign Up</button>
+                    </div>
+                </div>
+            </div>
         </div>
-       </div>
     );
 };
 
