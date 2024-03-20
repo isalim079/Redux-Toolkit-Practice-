82cc77dc6cc1a233405a9dceea9178a38fca5631
isalim079 <salimreza1025@gmail.com>
2024-03-20 13:41:57 +0600

    eslint error updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import { add } from "../../Redux/CartsSlice";
 import { useDispatch, useSelector } from "react-redux";
